c650f937e6c4cbf09c138b8774482ac48caa0fec
rosol111 <gulamrosul037@gmail.com>
2023-09-29 02:31:39 +0600

    Added by Gmail login and sing up or added by github ligin

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import app from "../../firebase/firebase.init";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+const Login = () => {
+  const [user, setUser] = useState(null);
+
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+  const handleGoogleSingIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleGithubSingIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSingOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      {user ? (
+        <button onClick={handleSingOut}>Sing out</button>
+      ) : (
+        <>
+          <button onClick={handleGoogleSingIn}>Google Login</button>
+          <button onClick={handleGithubSingIn}>Github Login</button>
+        </>
+      )}
+      {user && (
+        <div>
+          <h3>User: {user.displayName}</h3>
+          <p>Email: {user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_lIZVZCqbjqSuzrKiECyCSsgARI_KXBo",
+  authDomain: "simple-firebase-3e74e.firebaseapp.com",
+  projectId: "simple-firebase-3e74e",
+  storageBucket: "simple-firebase-3e74e.appspot.com",
+  messagingSenderId: "198885048270",
+  appId: "1:198885048270:web:9efb77ffe1b0bea64db59f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
